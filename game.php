f10afc48923fa6da4f073301da4f4f033e92245d
Nga Vu <jennievu8991@gmail.com>
2022-10-15 15:09:37 -0400

    update

--- game.php
+++ game.php
@@ -1,31 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-    <title>WHO WANTS TO BE A MILLIONAIRE?</title>
-    <link href="style.css" type="text/css" rel="stylesheet"/>
-</head>
-<body>
-	<p>
-		<form action = "<?php $_PHP_SELF ?>" method = "POST">
-        <p><?php echo $question  ?></p>
-		<input id="field_1" type="submit" name="Button" value="<?php echo $a ?>" />
-		<input id="field_2" type="submit" name="Button" value="<?php echo $b ?>" />
-		<input id="field_1" type="submit" name="Button" value="<?php echo $c ?>" />
-		<input id="field_2" type="submit" name="Button" value="<?php echo $d ?>" />
-      </form>
-		
-	</p>
-</body>
-</html>
-
 <?php
-	$num = rand(1,2);
-	$fh = fopen("questions.txt", "r");
-	while($line = fgets($fh)) {
-		$piece = explode(",", $line);
+	$fh = file("questions.txt");
+	$num = rand(1,count($fh));
+	$index = 0;
+	while ($index != count($fh)) {
+		$piece = explode(",", $fh[$num-1]);
 
-		if($piece[0] == $num) {
+		if ($piece[0] == $num) {
 			$question = $piece[1];
 			$a = $piece[2];
 			$b = $piece[3];
@@ -33,9 +13,35 @@
 			$d = $piece[5];
 			$correct = $piece[6];
 		}
-	}
-	if ($_POST['Button'] == $correct) {
-		echo "Answer correct";
+		$index++;
 	}
 ?>
 
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>WHO WANTS TO BE A MILLIONAIRE?</title>
+	<link href="style.css" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+	<form action="<?php $_PHP_SELF ?>" method="POST">
+		<p><?= $question  ?></p>
+		<div class="grid-question-container">
+			<input id="field_1" class="grid-item" name="Button" value="<?= $a ?>" />
+			<input id="field_2" class="grid-item" name="Button" value="<?= $b ?>" />
+			<input id="field_3" class="grid-item" name="Button" value="<?= $c ?>" />
+			<input id="field_4" class="grid-item" name="Button" value="<?= $d ?>" />
+		</div>
+		<input type="submit" />
+		<?php 
+			if (isset($_POST['Button']) && $_POST['Button'] == $correct) {
+				echo "Answer correct";
+			}	
+		?>
+	</form>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Who Want To Be A Millionaire?</title>
 </head>
 <body>
     <?php 
-        include('./question.html')
+        include('./main.php')
     ?>
 </body>
 </html>
