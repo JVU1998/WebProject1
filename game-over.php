5a45e487be92bd7ec0a1a76640dceb2bc29c94c3
Nga Vu <jennievu8991@gmail.com>
2022-10-15 20:02:15 -0400

    done!

---
+++ game-over.php
@@ -0,0 +1,5 @@
+<form action="index.php">
+    <div class="grid-question-container game-over">
+        <button id="game-over" class="game-submit-btn" type="submit">Game Over</button>
+    </div>
+</form>
---
+++ win-screen.php
@@ -0,0 +1,6 @@
+<form action="index.php">
+    <div class="grid-question-container back-to-home">
+        <h3>YOU WON!</h3>
+        <button id="back-to-home" class="game-submit-btn" type="submit">Back to homepage</button>
+    </div>
+</form>
