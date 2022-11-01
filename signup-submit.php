682d8f3be227b94448bebdc81bc05cc9b35f7fa9
Jennie Vu <jennievu8991@gmail.com>
2022-11-01 18:41:30 -0400

    fix duplicate user signup page

--- signup-submit.php
+++ signup-submit.php
@@ -12,20 +12,36 @@
  </body>
 </html>
 <?php
-    
         $filename = 'user-info.txt';
-        $myfile = fopen($filename, "a") or die("ERROR! Unable to open file!");
+        $userDup = false; 
+        foreach(file($filename) as $line) {
+            $l = explode(';', $line);
+            if(strcmp($l[0],$_POST["username"]) == 0) {
+                $userDup = true;
+            }
+        }
 
-        $info = $_POST["username"].";".$_POST["password"].";".$_POST["name"].";"."\n";
-                
-        fwrite($myfile, $info);
-        
-        fclose($myfile); 
+        if(!$userDup) {
+            $myfile = fopen($filename, "a") or die("ERROR! Unable to open file!");
+            
+            $info = $_POST["username"].";".$_POST["password"].";".$_POST["name"].";"."\n";
+                    
+            fwrite($myfile, $info);
+            
+            fclose($myfile); 
+        }
 ?>
 <div class="main">
-    Welcome <?= $_POST["name"] ?>, your account created sucessfully! <br>
-    <a href="main.php">
-                    <button class="button">Login to start playing!</button>    
-                </a>
+    <?php if($userDup) {?>
+        This username has been taken, 
+        <a href="signup.php">
+            <button class="button">please try again!</button>    
+        </a>
+    <?php } else { ?>
+        Welcome <?= $_POST["name"] ?>, your account created sucessfully! <br>
+        <a href="main.php">
+            <button class="button">Login to start playing!</button>    
+        </a>
+    <?php }?>
     
 </div>  
